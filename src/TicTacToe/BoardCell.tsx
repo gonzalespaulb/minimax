3b88d2595910c81b42405a7a32724038aa7bed71
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-03-15 09:53:34 -0700

    bot stops moving on win

--- src/TicTacToe/BoardCell.tsx
+++ src/TicTacToe/BoardCell.tsx
@@ -16,48 +16,53 @@ const BoardCell: FC<BoardCellProps> = ({
   const updatePositions = (player: string, chosenPosition: number) => {
     // NOTE -- CREATES A SHALLOW COPY OF  THE EXISTING BOARD
     const newBoard = boardPositions.slice();
-
-    // NOTE -- CHANGES NULL INTO WHICHEVER PLAYER
     const newOwner = newBoard[chosenPosition];
 
+    // NOTE -- IF ALREADY OWNED DISABLE CLICKING
     if (newOwner.ownedBy) {
       return false;
     } else {
+      // NOTE -- CHANGES NULL INTO WHICHEVER PLAYER
       newOwner.ownedBy = player;
       setBoardPositions(newBoard);
-      checkForWinners();
       return true;
     }
   };
 
   const botMove = () => {
-    setDisableMove(false);
-    // NOTE -- ONLY GIVES THE BOT ITEMS IN ARRAY THAT ARE NULL
-    let movesLeft: IBoardPositions[] = [];
-    boardPositions.map((position) => {
-      if (position.ownedBy) {
-        return;
-      } else {
-        movesLeft.push(position);
-      }
-    });
+    if (!checkForWinners()) {
+      setDisableMove(false);
+      // NOTE -- ONLY GIVES THE BOT ITEMS IN ARRAY THAT ARE NULL
+      let movesLeft: IBoardPositions[] = [];
 
-    // NOTE -- RANDOMIZES THE MOVES BOT CAN MAKE BASED ON THE UNFILLED SPACES
-    const botDesiredMove = Math.floor(Math.random() * movesLeft.length);
-    updatePositions(players.BOT, movesLeft[botDesiredMove].boardPosition);
-    setCurrentPlayer(players.BOT);
+      boardPositions?.map((position) => {
+        if (position.ownedBy) {
+          return;
+        } else {
+          movesLeft.push(position);
+        }
+      });
+
+      // NOTE -- STOPS BOT FROM MOVING WHEN THERE ARE NO MOVES LEFT
+      if (movesLeft.length > 1) {
+        const botDesiredMove = Math.floor(Math.random() * movesLeft.length);
+        updatePositions(players.BOT, movesLeft[botDesiredMove].boardPosition);
+        setCurrentPlayer(players.USER);
+      }
+    }
   };
 
   const userMove = () => {
-    if(updatePositions(players.USER, index)){
-      setCurrentPlayer(players.USER);
-      setDisableMove(true);
-      setTimeout(() => {
-        botMove();
-      }, 1000);
-    }
-    else {
-      return;
+    if (!checkForWinners()) {
+      // NOTE -- INDEX DETERMINES WHICH CELL IS BEING CLICKED
+      if (updatePositions(players.USER, index)) {
+        setCurrentPlayer(players.BOT);
+        setDisableMove(true);
+
+        setTimeout(() => {
+          botMove();
+        }, 1000);
+      }
     }
   };
 
@@ -69,7 +74,6 @@ const BoardCell: FC<BoardCellProps> = ({
     }
   };
 
-  // BUG -- OVERRIDES THE PREVIOUS USER'S MOVE WHEN CELL IS CLICKED TWICE. TIE ON THE LAST SQUARE ENABLES THIS TOO
   return (
     <Cell
       onClick={userMove}
